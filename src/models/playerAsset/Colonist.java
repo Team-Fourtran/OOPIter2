c0f0cb5ca439e5dd24d7e58fb75a9746de620707
CSchnars13 <cschnars13@ufl.edu>
2017-02-21 23:34:41 -0500

    Added some classes from last iteration that probably won't change much

---
+++ src/models/playerAsset/Colonist.java
@@ -0,0 +1,19 @@
+package application.models.playerAsset;
+
+//Unit type
+//Responsibilities: Weak attacker, can be consumed to make structure
+public class Colonist extends Unit{
+
+    public Colonist(){
+        offDamage = 0;
+        defDamage = 0;
+        armor = 0;
+        movesPerTurn = 2;
+        maxHealth = currentHealth = 100;
+        upkeep = 8;
+    }
+
+    public String getType(){
+        return "Colonist";
+    }
+}
---
+++ src/models/playerAsset/Unit.java
@@ -0,0 +1,25 @@
+package application.models.playerAsset;
+
+import application.models.commands.Command;
+import java.util.*;
+
+/* High level unit class used to define the Unit type
+	and define some basic functionality
+ */
+public abstract class Unit extends PlayerAsset{
+
+    protected int movesPerTurn; 	//how many tiles a unit can move in a turn
+
+    //Heal a unit by 50 health
+    public void heal(){
+        if (currentHealth + 50 > maxHealth)
+            currentHealth = maxHealth;
+        else
+            currentHealth += 50;
+    }
+
+    //gets unit type, overridden in subclasses
+    public String getType(){
+        return "this is a unit";
+    }
+}
