2bb3b0a9dc3ae662affc09c589c425d264931d9a
Juan Jauregui <juan@jauregui.co>
2017-02-24 16:05:10 -0500

    Waiting for iterators

---
+++ src/tests/TestCommandGen.java
@@ -0,0 +1,18 @@
+package tests;
+
+import controllers.*;
+
+import java.util.HashMap;
+
+public class TestCommandGen{
+    public static void main(String[] args){
+
+        /*
+        Waiting on AssetIterator to test Message Generation
+        KeyPressInformer keysim = new KeyPressInformer(new HashMap<String, Boolean>());
+        HashMap<String, AssetIterator> = new HashMap<>();
+        KeyboardController kbc = new KeyboardController(keySim, );
+        */
+    }
+
+}
