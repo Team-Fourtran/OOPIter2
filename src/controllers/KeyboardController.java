5670d7039eceb297e445848164556865f3da65af
Juan Jauregui <juan@jauregui.co>
2017-02-24 15:34:10 -0500

    Integrate Iteration 1's CommandGen

---
+++ src/controllers/KeyboardController.java
@@ -0,0 +1,23 @@
+package controllers;
+
+import java.util.HashMap;
+import java.util.Iterator;
+
+public class KeyboardController {
+    MessageGenerator msgGen;
+
+    public KeyboardController(KeyPressInformer keyInformer, HashMap<String, Iterator> assetIterators){
+        //Initializes the message generator, setting itself as the reciever, and forwarding the keysPressedList
+        this.msgGen = new MessageGenerator(this, keyInformer, assetIterators);
+    }
+
+    /* Happens when the player changes (aka turn switching) */
+    public void updateIterators(HashMap<String, Iterator> assetIterators){
+        this.msgGen.updateIterators(assetIterators);
+    }
+
+    /* TODO: For now, just prints messages that it receives */
+    protected void handleMsg(String msg){
+        System.out.println("From " + this + ":\n\tReceived message from MsgGen:\n\t\t" + msg);
+    }
+}
