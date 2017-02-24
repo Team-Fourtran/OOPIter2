a9459e574a5f7412011d2b48d569f57dd4d0cf8a
thkim1234 <tedkim1234@gmail.com>
2017-02-24 16:07:55 -0500

    Added game

---
+++ src/RunGame.java
@@ -0,0 +1,16 @@
+package application;/* Standard library imports */
+
+/* Main application packages */
+
+import application.Game;
+
+/*
+ * Named as per Dave's specifications 
+ * Contains the main method for creating and starting the game
+ */
+public class RunGame {
+    public static void main(String[] args){
+    	Game game = new Game();
+    	
+    }
+}
